e9a8e6cb9c32ffe66d5fd2a037b9ee0ba6e81d8c
SinthiaGonzalez <gonzalez.sinthia@icloud.com>
2023-12-15 22:17:42 +0100

    Serena hotel

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div>
+        <h1  className="text-red-500">Grupo 8 henrry </h1>
+      </div>
+      
+    
+  )
+}
+
+export default App
