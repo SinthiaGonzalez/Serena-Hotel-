e43a66fe3b1926c16846dc3955e1b0de2f05ec6a
pablofigueroa16 <pablofigueroa611@gmail.com>
2023-12-21 12:05:42 -0300

    configuracion tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import {BrowserRouter} from 'react-router-dom'
-import {Provider} from 'react-redux'
-import store from './redux/store/store.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store/store.js";
+import { ThemeProvider } from "@material-tailwind/react";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <Provider store={store}>
-      {/* Envuelve la aplicación con BrowserRouter para habilitar el enrutamiento */}
+    <Provider store={store}>
       <BrowserRouter>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
