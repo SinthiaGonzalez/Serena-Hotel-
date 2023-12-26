226f86b9148115bf052b34fdcf09ac9a3f732924
alej098 <urzola2003@gmail.com>
2023-12-26 12:50:35 -0500

    comflicto comfigurado de app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,6 @@ import "./App.css";
 import LandingPage from "./Componentes/LandingPage/LandingPage.jsx";
 import ComentPage from "./Componentes/comentpage/comentpage.jsx"
 import CreateComentPage from "./Componentes/CreateComentario/CreateComentario.jsx";
-import { Route, Routes } from "react-router-dom";
-
-const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/comentarios" element={< ComentPage />} />
-        <Route path="/comentar" element={< CreateComentPage />} />
-      </Routes>
-    </>
-  );
-};
-
-export default App;
-
-import "./App.css";
-import LandingPage from "./Componentes/LandingPage/LandingPage.jsx";
 import Error404 from "./Componentes/Error 404/Error404.jsx";
 import { Route, Routes } from "react-router-dom";
 
@@ -33,8 +15,10 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        {/* La siguiente ruta captura cualquier otra ruta y muestra el componente 404 */}
-        <Route path="*" element={<Error404 />} />
+        <Route path="/comentarios" element={< ComentPage />} />
+        <Route path="/comentar" element={< CreateComentPage />} />
+         {/* La siguiente ruta captura cualquier otra ruta y muestra el componente 404 */}
+         <Route path="*" element={<Error404 />} />
       </Routes>
     </>
   );
