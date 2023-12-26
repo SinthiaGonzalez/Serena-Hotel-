6113019c48f18e0fffafaa757edab49fd39931f7
alej098 <urzola2003@gmail.com>
2023-12-26 12:49:52 -0500

    solucion del conflicto

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,26 @@
 import "./App.css";
 import LandingPage from "./Componentes/LandingPage/LandingPage.jsx";
+import ComentPage from "./Componentes/comentpage/comentpage.jsx"
+import CreateComentPage from "./Componentes/CreateComentario/CreateComentario.jsx";
+import { Route, Routes } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/comentarios" element={< ComentPage />} />
+        <Route path="/comentar" element={< CreateComentPage />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
+
+import "./App.css";
+import LandingPage from "./Componentes/LandingPage/LandingPage.jsx";
+import Error404 from "./Componentes/Error 404/Error404.jsx";
 import { Route, Routes } from "react-router-dom";
 
 // Esta linea de codigo hace que por default todos los requerimientos en axios se hagan a esta ruta en el back
@@ -14,8 +35,6 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         {/* La siguiente ruta captura cualquier otra ruta y muestra el componente 404 */}
         <Route path="*" element={<Error404 />} />
-        <Route path="/comentarios" element={< ComentPage />} />
-        <Route path="/comentar" element={< CreateComentPage />} />
       </Routes>
     </>
   );
