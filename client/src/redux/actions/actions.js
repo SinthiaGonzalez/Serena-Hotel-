3f0c667b4036c82a540a894a85e072c0a04a5c2b
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-01-03 16:35:08 -0300

    agrreglo error

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,13 +1,69 @@
 import axios from "axios";
+import { GET_COMENTARIOS, ELIMINAR_COMENTARIO } from "./actions-types";
 
-export function getHabitaciones() {
-  console.log("estoy en actions")
-  return async function(dispatch) {
+export function postComent(state) {
+  return async function (dispatch) {
     try {
-      const habitaciones = await axios.get('/habitaciones');
+      await axios.post("/comentar", state);
+      alert("se añadió el comentario exitosamente");
+    } catch (error) {
+      alert(error);
+    }
+  };
+}
+export function postUsuario(state) {
+  return async function (dispatch) {
+    try {
+      await axios.post("/usuario", state);
+      console.log("log de action", state);
+      alert("se creo el usuario exitosamente");
+    } catch (error) {
+      alert(error);
+    }
+  };
+}
+
+export function getAllcomentarios() {
+  return async function (dispatch) {
+    try {
+      const response = await axios.get("/comentarios");
+      if (response.status === 200) {
+        dispatch({
+          type: GET_COMENTARIOS,
+          payload: response.data,
+        });
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export function eliminarComentario(id) {
+  return async function (dispatch) {
+    try {
+      const response = await axios.delete(`/comentario/${id}`);
+      console.log("log de actions", response);
+      if (response.status === 200) {
+        dispatch({
+          type: ELIMINAR_COMENTARIO,
+          payload: id,
+        });
+        alert("Comentario eliminado exitosamente");
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+export function getHabitaciones() {
+  console.log("estoy en actions");
+  return async function (dispatch) {
+    try {
+      const habitaciones = await axios.get("/habitaciones");
       return dispatch({
-        type: 'GET_HABITACIONES',
-        payload: habitaciones.data
+        type: "GET_HABITACIONES",
+        payload: habitaciones.data,
       });
     } catch (error) {
       console.log(error);
@@ -17,12 +73,15 @@ export function getHabitaciones() {
 
 // creamos la action que crea la preferenciaId de mercadopago
 export function createPreferenceMercadopagoId(data) {
-  return async function(dispatch) {
+  return async function (dispatch) {
     try {
-      const preference = await axios.post("mercadopago/create_preference", data);
+      const preference = await axios.post(
+        "mercadopago/create_preference",
+        data
+      );
       return dispatch({
-        type: 'CREATE_PREFERENCE_MERCADOPAGO_ID',
-        payload: preference.data
+        type: "CREATE_PREFERENCE_MERCADOPAGO_ID",
+        payload: preference.data,
       });
     } catch (error) {
       console.log(error);
@@ -34,7 +93,7 @@ export function createPreferenceMercadopagoId(data) {
 export function addToCart(id) {
   return {
     type: "ADD_TO_CART",
-    payload: id
+    payload: id,
   };
 }
 
