f26f57ee01a765d2529a40bc4b4c50e7d580f34a
Joan Laurente <Joan2022Laurente>
2024-01-08 17:00:23 -0500

    avance 3

--- client/src/Componentes/PerfilUsuario/PerfilUsuario.jsx
+++ client/src/Componentes/PerfilUsuario/PerfilUsuario.jsx
@@ -1,50 +1,65 @@
 import React, { useState } from "react";
+import { UserIcon, EnvelopeIcon, DevicePhoneMobileIcon, LockClosedIcon, LockOpenIcon} from "@heroicons/react/24/solid";
 export const Perfilusuario1 = () => {
   return (
     <>
       <div className="h-[120vh] flex flex-col items-center justify-center">
         <div className="bg-verde p-12 rounded-lg shadow-md  text-white">
           <h2 className="text-2xl font-semibold mb-4">Perfil</h2>
-          <div className="w-16 h-16 rounded-full bg-white mx-auto mb-10">
-          </div>
+          <div className="w-16 h-16 rounded-full bg-white mx-auto mb-10"></div>
 
           <div className="mb-4 bg-white bg-opacity-25 p-2  flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Nombre:
-            </label>
+            <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <UserIcon width={30} />
+                Nombre:
+              </label>
+            </div>
             <p className="text-white-800 pl-3 w-1/2 text-center">
               Nombre de Usuario
             </p>
           </div>
           <div className="mb-4 bg-white bg-opacity-25 p-2  flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Apellido:
-            </label>
+          <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <UserIcon width={30} />
+                Apellido:
+              </label>
+            </div>
             <p className="text-white-800 pl-3 w-1/2 text-center">
               Apellido de Usuario
             </p>
           </div>
 
           <div className="mb-4 bg-white bg-opacity-25 p-2  flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Correo:
-            </label>
+          <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <EnvelopeIcon width={30} />
+                Correo:
+              </label>
+            </div>
             <p className="text-white-800 pl-3 w-1/2 text-center">
               correo@gmail.com
             </p>
           </div>
 
           <div className="mb-4 bg-white bg-opacity-25 p-2  flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Telefono:
-            </label>
+          <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <DevicePhoneMobileIcon width={30} />
+                Telefono:
+              </label>
+            </div>
             <p className="text-white-800 pl-3 w-1/2 text-center">1515115</p>
           </div>
 
           <div className="mb-4 bg-white bg-opacity-25 p-2 flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Contraseña:
-            </label>
+          <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <LockClosedIcon width={30} />
+                Contraseña:
+              </label>
+            </div>
             <input
               type="password"
               className="text-white-800 pl-3 w-1/2 text-center bg-transparent border-none outline-none"
@@ -53,9 +68,12 @@ export const Perfilusuario1 = () => {
           </div>
 
           <div className="mb-4 bg-white bg-opacity-25 p-2 flex items-center w-[600px]">
-            <label className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
-              Repetir Contraseña:
-            </label>
+          <div className="block text-white-600 text-sm font-medium pr-2 border-r-2 border-naranja w-1/2 text-center">
+              <label className="flex items-center justify-center">
+                <LockClosedIcon width={30} />
+                Repetir Contraseña:
+              </label>
+            </div>
             <input
               type="password"
               className="text-white-800 pl-3 w-1/2 text-center bg-transparent border-none outline-none"
@@ -69,6 +87,9 @@ export const Perfilusuario1 = () => {
             </button>
             <button className="bg-naranja text-white py-2 px-4 rounded-md hover:bg-orange-600 focus:outline-none focus:shadow-outline-orange">
               Eliminar Cuenta
+            </button>
+            <button className="bg-naranja text-white py-2 px-4 rounded-md hover:bg-orange-600 focus:outline-none focus:shadow-outline-orange">
+              Guardar cambios
             </button>
           </div>
         </div>
