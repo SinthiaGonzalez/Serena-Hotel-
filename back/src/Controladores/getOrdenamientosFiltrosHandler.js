e3d1a0ba315fdfb336a9f1f2a75c46738886a01e
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-01-09 19:34:18 -0300

    ordenamientos y filtros en back-end

--- back/src/Controladores/getOrdenamientosFiltrosHandler.js
+++ back/src/Controladores/getOrdenamientosFiltrosHandler.js
@@ -2,9 +2,9 @@ const { Habitaciones } = require("../db");
 
 const getOrdenamientosFiltrosHandler = async (req, res) => {
   try {
-    const { ordenarPor, direccion } = req.query;
+    const { ordenarPor, direccion, filtroPersonas } = req.query;
 
-    const ordenValido = ["nombre", "precio", "createdAt"].includes(ordenarPor);
+    const ordenValido = ["nombre", "precio"].includes(ordenarPor);
     const direccionValida = ["asc", "desc"].includes(direccion.toLowerCase());
 
     if (!ordenValido || !direccionValida) {
@@ -13,15 +13,26 @@ const getOrdenamientosFiltrosHandler = async (req, res) => {
         .json({ error: "Parámetros de ordenamiento no válidos" });
     }
 
-    // Construir la consulta de Sequelize con orden y dirección
     const consulta = {
       order: [[ordenarPor, direccion]],
     };
 
-    const habitacionesOrdenadas = await Habitaciones.findAll(consulta);
+    let habitacionesOrdenadas = await Habitaciones.findAll(consulta);
 
+    if (filtroPersonas) {
+      habitacionesOrdenadas = habitacionesOrdenadas.filter((habitacion) => {
+        const servicios = habitacion.servicios || [];
+        const personas = servicios.find(
+          (servicio) => servicio.descripcion === `${filtroPersonas} pers`
+        );
+        return personas;
+      });
+    }
+
+    console.log("Habitaciones filtradas:", habitacionesOrdenadas);
     res.status(200).json(habitacionesOrdenadas);
   } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({ error: error.message });
   }
 };
