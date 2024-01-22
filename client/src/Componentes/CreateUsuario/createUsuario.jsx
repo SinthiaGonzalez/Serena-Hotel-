76ab7c11349157dbf423f6d1b9fd3c90a9ef6098
Franco <famularifranco@gmail.com>
2024-01-22 18:09:40 -0300

    prueba deploy

--- client/src/Componentes/CreateUsuario/createUsuario.jsx
+++ client/src/Componentes/CreateUsuario/createUsuario.jsx
@@ -1,253 +1,253 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faUser,
-  faEnvelope,
-  faPhone,
-  faLock,
-} from "@fortawesome/free-solid-svg-icons";
-import { postUsuario } from "../../redux/actions/actions";
+// import React, { useState } from "react";
+// import { useDispatch } from "react-redux";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import {
+//   faUser,
+//   faEnvelope,
+//   faPhone,
+//   faLock,
+// } from "@fortawesome/free-solid-svg-icons";
+// import { postUsuario } from "../../redux/actions/actions";
 
-const CreateUsuario = () => {
-  const dispatch = useDispatch();
+// const CreateUsuario = () => {
+//   const dispatch = useDispatch();
 
-  const [user, setUser] = useState({
-    name: "",
-    apellido: "",
-    email: "",
-    telefono: "",
-    contraseña: "",
-  });
+//   const [user, setUser] = useState({
+//     name: "",
+//     apellido: "",
+//     email: "",
+//     telefono: "",
+//     contraseña: "",
+//   });
 
-  const handleChange = (e) => {
-    setUser({
-      ...user,
-      [e.target.name]: e.target.value,
-    });
-  };
+//   const handleChange = (e) => {
+//     setUser({
+//       ...user,
+//       [e.target.name]: e.target.value,
+//     });
+//   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await dispatch(postUsuario(user));
-      // Restablecer el estado a los valores iniciales en lugar de un objeto vacío
-      setUser({
-        name: "",
-        apellido: "",
-        email: "",
-        telefono: "",
-        contraseña: "",
-      });
-    } catch (error) {
-      alert(error.message);
-    }
-  };
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       await dispatch(postUsuario(user));
+//       // Restablecer el estado a los valores iniciales en lugar de un objeto vacío
+//       setUser({
+//         name: "",
+//         apellido: "",
+//         email: "",
+//         telefono: "",
+//         contraseña: "",
+//       });
+//     } catch (error) {
+//       alert(error.message);
+//     }
+//   };
 
-  return (
-    <div
-      className="relative bg-cover bg-center text-white text-center p-8 h-screen"
-      style={{
-        backgroundImage:
-          'url("https://i.postimg.cc/3xxjwxft/selena-hotel-1.png")',
-      }}
-    >
-      <div className="flex flex-col items-center justify-center h-auto bg-blanco w-2/3 rounded-lg px-20 mx-[250px] px-4 pt-3 pb-6">
-        <a
-          href="/logearse"
-          className="font-inter text-base antialiased font-bold text-naranja text-inter hover:scale-105 w-1/6 ml-[-700px] mt-6"
-        >
-          🡰 Volver
-        </a>
-        <p className="flex mt-4 font-inter text-3xl antialiased leading-normal text-center font-bold text-gris justify-center">
-          Registrarse
-        </p>
-        <form onSubmit={handleSubmit} className="w-2/3">
-          <h2 className="text-2xl mb-4">Crear Usuario</h2>
-          <div className="mb-4">
-            <label className="block text-gray-200 text-sm font-bold mb-2">
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//   return (
+//     <div
+//       className="relative bg-cover bg-center text-white text-center p-8 h-screen"
+//       style={{
+//         backgroundImage:
+//           'url("https://i.postimg.cc/3xxjwxft/selena-hotel-1.png")',
+//       }}
+//     >
+//       <div className="flex flex-col items-center justify-center h-auto bg-blanco w-2/3 rounded-lg px-20 mx-[250px] px-4 pt-3 pb-6">
+//         <a
+//           href="/logearse"
+//           className="font-inter text-base antialiased font-bold text-naranja text-inter hover:scale-105 w-1/6 ml-[-700px] mt-6"
+//         >
+//           🡰 Volver
+//         </a>
+//         <p className="flex mt-4 font-inter text-3xl antialiased leading-normal text-center font-bold text-gris justify-center">
+//           Registrarse
+//         </p>
+//         <form onSubmit={handleSubmit} className="w-2/3">
+//           <h2 className="text-2xl mb-4">Crear Usuario</h2>
+//           <div className="mb-4">
+//             <label className="block text-gray-200 text-sm font-bold mb-2">
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faUser}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="text"
-                  name="name"
-                  placeholder="Nombre"
-                  value={user.name}
-                  onChange={handleChange}
-                />
-              </div>
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="text"
+//                   name="name"
+//                   placeholder="Nombre"
+//                   value={user.name}
+//                   onChange={handleChange}
+//                 />
+//               </div>
 
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faUser}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faUser}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="text"
-                  name="apellido"
-                  placeholder="Apellido"
-                  value={user.apellido}
-                  onChange={handleChange}
-                />
-              </div>
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="text"
+//                   name="apellido"
+//                   placeholder="Apellido"
+//                   value={user.apellido}
+//                   onChange={handleChange}
+//                 />
+//               </div>
 
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faEnvelope}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faEnvelope}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="email"
-                  name="email"
-                  placeholder="Email"
-                  value={user.email}
-                  onChange={handleChange}
-                />
-              </div>
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="email"
+//                   name="email"
+//                   placeholder="Email"
+//                   value={user.email}
+//                   onChange={handleChange}
+//                 />
+//               </div>
 
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faPhone}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faPhone}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="text"
-                  name="telefono"
-                  placeholder="Teléfono"
-                  value={user.telefono}
-                  onChange={handleChange}
-                />
-              </div>
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="text"
+//                   name="telefono"
+//                   placeholder="Teléfono"
+//                   value={user.telefono}
+//                   onChange={handleChange}
+//                 />
+//               </div>
 
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faLock}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faLock}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="password"
-                  name="contraseña"
-                  placeholder="Contraseña"
-                  value={user.contraseña}
-                  onChange={handleChange}
-                />
-              </div>
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="password"
+//                   name="contraseña"
+//                   placeholder="Contraseña"
+//                   value={user.contraseña}
+//                   onChange={handleChange}
+//                 />
+//               </div>
 
-              <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
-                <div className="items-center">
-                  <FontAwesomeIcon
-                    icon={faLock}
-                    className="w-[45px] h-7 p-2 border-r-4 border-naranja"
-                  />
-                </div>
+//               <div className="flex flex-row h-11 bg-verde  relative rounded-lg mb-4">
+//                 <div className="items-center">
+//                   <FontAwesomeIcon
+//                     icon={faLock}
+//                     className="w-[45px] h-7 p-2 border-r-4 border-naranja"
+//                   />
+//                 </div>
 
-                <input
-                  className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
-                  type="password"
-                  name="confirmarContraseña"
-                  placeholder="Confirmar Contraseña"
-                  value={user.confirmarContraseña}
-                  onChange={handleChange}
-                />
-              </div>
-            </label>
-          </div>
-          <button
-            className="w-2/4 mb-4 mt-4 select-none rounded-lg bg-naranja py-3.5 px-7 text-center align-middle font-inter text-base font-bold uppercase text-blanco transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-naranja hover:border-blanco"
-            type="button"
-            onClick={handleSubmit}
-          >
-            REGISTRATE
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-};
+//                 <input
+//                   className="w-full h-11 font-inter text-center pr-24 text-base font-normal text-white bg-verde rounded-lg"
+//                   type="password"
+//                   name="confirmarContraseña"
+//                   placeholder="Confirmar Contraseña"
+//                   value={user.confirmarContraseña}
+//                   onChange={handleChange}
+//                 />
+//               </div>
+//             </label>
+//           </div>
+//           <button
+//             className="w-2/4 mb-4 mt-4 select-none rounded-lg bg-naranja py-3.5 px-7 text-center align-middle font-inter text-base font-bold uppercase text-blanco transition-all focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none border-2 border-naranja hover:border-blanco"
+//             type="button"
+//             onClick={handleSubmit}
+//           >
+//             REGISTRATE
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
 
-export default CreateUsuario;
+// export default CreateUsuario;
 
-{
-  /* <FontAwesomeIcon icon={faUser} className="mr-2" />
-<input
-  type="text"
-  name="name"
-  placeholder="Nombre"
-  value={user.name}
-  onChange={handleChange}
-  className="appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
-/> */
-}
-{
-  /* </label>
-<label className="block text-gray-200 text-sm font-bold mb-2">
-<FontAwesomeIcon icon={faUser} className="mr-2" />
-<input
-  type="text"
-  name="apellido"
-  placeholder="Apellido"
-  value={user.apellido}
-  onChange={handleChange}
-  className="appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
-/> */
-}
-{
-  /* </label>
-<label className="block text-gray-200 text-sm font-bold mb-2">
-<FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-<input
-  type="email"
-  name="email"
-  placeholder="Email"
-  value={user.email}
-  onChange={handleChange}
-  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
-/> */
-}
-{
-  /* </label>
-<label className="block text-gray-200 text-sm font-bold mb-2">
-<FontAwesomeIcon icon={faPhone} className="mr-2" />
-<input
-  type="text"
-  name="telefono"
-  placeholder="Teléfono"
-  value={user.telefono}
-  onChange={handleChange}
-  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
-/>
-</label>
-<label className="block text-gray-200 text-sm font-bold mb-2">
-<FontAwesomeIcon icon={faLock} className="mr-2" />
-<input
-  type="password"
-  name="contraseña"
-  placeholder="Contraseña"
-  value={user.contraseña}
-  onChange={handleChange}
-  className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
-/> */
-}
+// {
+//   /* <FontAwesomeIcon icon={faUser} className="mr-2" />
+// <input
+//   type="text"
+//   name="name"
+//   placeholder="Nombre"
+//   value={user.name}
+//   onChange={handleChange}
+//   className="appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
+// /> */
+// }
+// {
+//   /* </label>
+// <label className="block text-gray-200 text-sm font-bold mb-2">
+// <FontAwesomeIcon icon={faUser} className="mr-2" />
+// <input
+//   type="text"
+//   name="apellido"
+//   placeholder="Apellido"
+//   value={user.apellido}
+//   onChange={handleChange}
+//   className="appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
+// /> */
+// }
+// {
+//   /* </label>
+// <label className="block text-gray-200 text-sm font-bold mb-2">
+// <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+// <input
+//   type="email"
+//   name="email"
+//   placeholder="Email"
+//   value={user.email}
+//   onChange={handleChange}
+//   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
+// /> */
+// }
+// {
+//   /* </label>
+// <label className="block text-gray-200 text-sm font-bold mb-2">
+// <FontAwesomeIcon icon={faPhone} className="mr-2" />
+// <input
+//   type="text"
+//   name="telefono"
+//   placeholder="Teléfono"
+//   value={user.telefono}
+//   onChange={handleChange}
+//   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
+// />
+// </label>
+// <label className="block text-gray-200 text-sm font-bold mb-2">
+// <FontAwesomeIcon icon={faLock} className="mr-2" />
+// <input
+//   type="password"
+//   name="contraseña"
+//   placeholder="Contraseña"
+//   value={user.contraseña}
+//   onChange={handleChange}
+//   className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-[#1D2828] text-white"
+// /> */
+// }
