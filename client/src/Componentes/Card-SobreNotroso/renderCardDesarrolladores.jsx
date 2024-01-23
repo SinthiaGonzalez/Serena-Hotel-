df8cd510677449a1e01caefb9c5cbcda0ce92aa8
Franco <famularifranco@gmail.com>
2024-01-23 12:12:10 -0300

    cards developer BD

--- client/src/Componentes/Card-SobreNotroso/renderCardDesarrolladores.jsx
+++ client/src/Componentes/Card-SobreNotroso/renderCardDesarrolladores.jsx
@@ -4,70 +4,70 @@ import { getDevs } from "../../redux/Actions/actions";
 import { useEffect } from "react";
 
 const RenderCardDesarrolladores = () => {
-  // const developers = useSelector((state) => state.developers);
-  // const dispatch = useDispatch();
+  const developers = useSelector((state) => state.developers);
+  const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(getDevs());
+  useEffect(() => {
+    dispatch(getDevs());
 
-  // }, []);
+  }, []);
 
-  const developers = [
-    {
-      id: 1,
-      nombre: "Sithia Gonzalez",
-      ocupacion: "Full Stack Developer",
-      imagen: "https://avatars.githubusercontent.com/u/128981283?v=4",
-      imagenFondo:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/SinthiaGonzalez",
-      linkedin: "https://www.linkedin.com/in/sinthia-fabiana-gonzalez/",
-    },
-    {
-      id: 2,
-      nombre: "Pablo Figueroa",
-      ocupacion: "Full Stack Developer",
-      imagen: "https://i.postimg.cc/zfZY85RH/Pablo-Perfil.jpg",
-      imagenFondo:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/pablofigueroa16",
-      linkedin: "https://www.linkedin.com/in/pablo-figueroa-0b204b22a/",
-    },
-    {
-      id: 3,
-      nombre: "Franco Famulari",
-      ocupacion: "Full Stack Developer",
-      imagen: "https://i.postimg.cc/7hMsC92d/Foto-CV.png",
-      imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/Francofamu",
-      linkedin: "https://www.linkedin.com/in/franco-famulari-25b2b9127/",
-    },
-    {
-      id: 4,
-      nombre: "Jesus Vargas",
-      ocupacion: "Full Stack Developer",
-      imagen:"https://i.postimg.cc/BnGLMxRn/curriculum-fotor-bg-remover-20240111163843.png",
-      imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/jesusavargasr",
-      linkedin: "https://www.linkedin.com/in/jesus-vargas-bb4a10217/",
-    },
-    {
-      id: 5,
-      nombre: "Alejandro Becerra",
-      ocupacion: "Full Stack Developer",
-      imagen: "https://i.postimg.cc/Z5d1Jpmg/00d8a4e1-fdcc-4bea-aabe-45a8d0da6c69.jpg",
-      imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/alej098",
-      linkedin: "https://www.linkedin.com/in/d-alejandro-becerra-g-93319025a/",
-    },
-    {
-      id: 6,
-      nombre: "Verónica Di Maria",
-      ocupacion: "Full Stack Developer",
-      imagen: "https://i.postimg.cc/1z900qrF/cv-img-2.jpg",
-      imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
-      github: "https://github.com/VeronicaBDev",
-      linkedin: "https://www.linkedin.com/in/d-alejandro-becerra-g-93319025a/",
-    },
-  ];
+  // const developers = [
+  //   {
+  //     id: 1,
+  //     nombre: "Sithia Gonzalez",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen: "https://avatars.githubusercontent.com/u/128981283?v=4",
+  //     imagenFondo:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/SinthiaGonzalez",
+  //     linkedin: "https://www.linkedin.com/in/sinthia-fabiana-gonzalez/",
+  //   },
+  //   {
+  //     id: 2,
+  //     nombre: "Pablo Figueroa",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen: "https://i.postimg.cc/zfZY85RH/Pablo-Perfil.jpg",
+  //     imagenFondo:"https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/pablofigueroa16",
+  //     linkedin: "https://www.linkedin.com/in/pablo-figueroa-0b204b22a/",
+  //   },
+  //   {
+  //     id: 3,
+  //     nombre: "Franco Famulari",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen: "https://i.postimg.cc/7hMsC92d/Foto-CV.png",
+  //     imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/Francofamu",
+  //     linkedin: "https://www.linkedin.com/in/franco-famulari-25b2b9127/",
+  //   },
+  //   {
+  //     id: 4,
+  //     nombre: "Jesus Vargas",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen:"https://i.postimg.cc/BnGLMxRn/curriculum-fotor-bg-remover-20240111163843.png",
+  //     imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/jesusavargasr",
+  //     linkedin: "https://www.linkedin.com/in/jesus-vargas-bb4a10217/",
+  //   },
+  //   {
+  //     id: 5,
+  //     nombre: "Alejandro Becerra",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen: "https://i.postimg.cc/Z5d1Jpmg/00d8a4e1-fdcc-4bea-aabe-45a8d0da6c69.jpg",
+  //     imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/alej098",
+  //     linkedin: "https://www.linkedin.com/in/d-alejandro-becerra-g-93319025a/",
+  //   },
+  //   {
+  //     id: 6,
+  //     nombre: "Verónica Di Maria",
+  //     ocupacion: "Full Stack Developer",
+  //     imagen: "https://i.postimg.cc/1z900qrF/cv-img-2.jpg",
+  //     imagenFondo: "https://cf.bstatic.com/xdata/images/hotel/max1024x768/283384657.jpg?k=82f12511a23fc911e79146601860d7ae7b9839f37af39918d1312edd9d98efee&o=&hp=1",
+  //     github: "https://github.com/VeronicaBDev",
+  //     linkedin: "https://www.linkedin.com/in/d-alejandro-becerra-g-93319025a/",
+  //   },
+  // ];
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 px-28 bg-white  ml-5 mr-5 md:ml-5 md:mr-5 lg:ml-10 lg:mr-10 xl:ml-28 xl:mr-28">
