9452eb805e8d00fe1d389b60a036bf4cd5753afe
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-01-26 17:57:37 -0300

    cambios

--- client/src/Componentes/Boton-MercadoPago/BotonMP.jsx
+++ client/src/Componentes/Boton-MercadoPago/BotonMP.jsx
@@ -6,13 +6,10 @@ const BotonMercadoPago = () => {
   const preferenceIdMP = useSelector((state) => state.preferenceIdMP);
 
   return (
-    <div className="bg-naranja w-2/3 h-1/3">
-      <p>Boton</p>
-      <div className="opacity-0 z-10 ">
-        <Wallet
-          initialization={{ preferenceId: preferenceIdMP.id, locale: "es-AR" }}
-        />
-      </div>
+    <div>
+      <Wallet
+        initialization={{ preferenceId: preferenceIdMP.id, locale: "es-AR" }}
+      />
     </div>
   );
 };
