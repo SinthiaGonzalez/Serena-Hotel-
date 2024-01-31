05b380ba63e6b32fcdec4a39d1d0226fe5ee53a7
pablofigueroa16 <pablofigueroa611@gmail.com>
2024-01-31 01:19:32 -0300

    cambioss

--- client/src/Componentes/CardShopHabitaciones/CardShopHabitaciones.jsx
+++ client/src/Componentes/CardShopHabitaciones/CardShopHabitaciones.jsx
@@ -83,7 +83,6 @@ const CardShopHabitaciones = ({
       didOpen: (toast) => {
         toast.onmouseenter = Swal.stopTimer;
         toast.onmouseleave = Swal.resumeTimer;
-
       },
     });
     Toast.fire({
@@ -92,12 +91,11 @@ const CardShopHabitaciones = ({
     });
   };
 
-
   return (
     <>
       <div
         key={id}
-        className="flex w-full h-60 flex-row items-center justify-between rounded-xl bg-verde bg-clip-border text-blanco"
+        className="flex w-full h-auto 2xl:h-60 py-2 flex-col md:flex-row items-center justify-between rounded-xl bg-verde bg-clip-border text-blanco"
       >
         <div className="h-44 mx-8 my-8 w-60 overflow-hidden text-white  rounded-xl bg-verde bg-clip-border  hover:scale-105">
           <Link to={`/habitacion/${id}`}>
@@ -109,7 +107,7 @@ const CardShopHabitaciones = ({
           </Link>
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-6">
+        <div className="flex flex-col items-center justify-center gap-2 2xl:gap-6 text-center">
           <Link
             to={`/habitacion/${id}`}
             className="block font-sans text-2xl antialiased font-bold leading-snug tracking-normal text-blanco hover:scale-105"
@@ -119,7 +117,7 @@ const CardShopHabitaciones = ({
           <h5 className="block font-sans text-xl antialiased font-bold leading-snug tracking-normal text-blanco">
             {tipo}
           </h5>
-          <div className="flex flex-row gap-4">
+          <div className="grid grid-cols-3 lg:flex xl:flex-row gap-4">
             {servicios.map(({ icono, descripcion }, index) => (
               <div key={`${icono}-${descripcion}-${index}`}>
                 <span className="material-symbols-outlined cursor-pointer rounded-full border border-verde bg-gray-900/5 p-3 text-blanco transition-colors hover:border-blanco hover:bg-gray-900/10 hover:!opacity-100 group-hover:opacity-70">
@@ -131,8 +129,8 @@ const CardShopHabitaciones = ({
           </div>
         </div>
 
-        <div className="p-6 pt-3 flex flex-col items-center justify-center gap-4">
-          <p className="text-2xl font-bold text-blanco">
+        <div className="p-6 pt-3 flex flex-col items-center justify-center gap-4 text-center">
+          <p className="text-2xl font-bold text-blanco py-4 md:py-0">
             {precio.toLocaleString("es-AR", {
               style: "currency",
               currency: "ARS",
@@ -206,6 +204,5 @@ const CardShopHabitaciones = ({
     </>
   );
 };
-
 
 export default CardShopHabitaciones;
